6d3c9542b858042f920f0f9bbcb21b4907b48b16
Vincent <vincent.brunet27@gmail.com>
2024-02-05 16:18:01 +0100

    Enlevage de l'authentification

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from auth.authenticator import Authenticator
 from home import run_home
 
-authenticator = Authenticator()
+#authenticator = Authenticator()
 
 def main():
-    name, authentication_status, username = authenticator.login()
+    """name, authentication_status, username = authenticator.login()
     role = authenticator.get_role(username)
 
     if authentication_status:
@@ -17,6 +17,8 @@ def main():
 
     elif authentication_status is None:
         st.warning('Veuillez entrer votre nom d\'utilisateur et mot de passe')
+    """
+    run_home(None, 'admin')
 
 if __name__ == '__main__':
     main()
